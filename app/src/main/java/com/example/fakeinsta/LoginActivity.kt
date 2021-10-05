ff6b25637333aca1e8d95faa7adcaf81177847c1
nop <dudu.arttam@gmail.com>
2021-10-05 19:38:39 +0330

    add drawer and nav to main activity ...

---
+++ app/src/main/java/com/example/fakeinsta/LoginActivity.kt
@@ -0,0 +1,57 @@
+package com.example.fakeinsta
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.AutoCompleteTextView
+import android.widget.EditText
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.google.android.material.snackbar.Snackbar
+import org.json.JSONObject
+
+class LoginActivity : AppCompatActivity() {
+    private val url = "http://192.168.1.101/lara/public/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val email = findViewById<AutoCompleteTextView>(R.id.email_auto_complete_text)
+        val password = findViewById<EditText>(R.id.password_edit_text)
+
+        logIn(email.text.toString(), password.text.toString())
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    private fun logIn(email: String, password: String) {
+        val q = Volley.newRequestQueue(this)
+
+        val view = this.findViewById<RecyclerView>(R.id.recyclerview)
+
+        val jobj = JSONObject()
+        jobj.put("email", email)
+        jobj.put("password", password)
+        val req = JsonObjectRequest(
+            Request.Method.POST,
+            url + "api/login",
+            null,
+            { response ->
+                val status = response.getString("status")
+                val message = response.getString("message")
+                if (status === "success") {
+                    val editor = applicationContext.getSharedPreferences("login", 0).edit()
+                    editor.putString("token", response.getString("token"))
+                    editor.apply()
+                    finish()
+                } else {
+                    Snackbar.make(view, message, 6000).show()
+                }
+            },
+            { error -> Snackbar.make(view, error.message.toString(), 6000).show() }
+        )
+
+    }
+}
--- app/src/main/java/com/example/fakeinsta/MainActivity.kt
+++ app/src/main/java/com/example/fakeinsta/MainActivity.kt
@@ -1,24 +1,73 @@
 package com.example.fakeinsta
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+    private val url = "http://192.168.1.101/lara/public/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpToolbar()
+        setUpRecyclerView()
+    }
 
+    private fun setUpRecyclerView() {
         val postLoader: PostLoader = PostLoader(
             this,
             this.findViewById(R.id.recyclerview),
             Volley.newRequestQueue(this),
-            "http://192.168.1.101/lara/public/"
+            url
         )
         // TODO : Add Paginate to App.
         postLoader.Load()
-
     }
+
+    private fun setUpToolbar() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        this.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        this.supportActionBar?.setHomeButtonEnabled(true)
+        // Set up Drawer
+        val drawer = findViewById<DrawerLayout>(R.id.drawer)
+        ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            0,
+            0
+        ).syncState()
+    }
+
+    private fun isLogin(): Boolean {
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.main_menu, menu)
+
+//        if (isLogin())
+//        {
+//            menu?.findItem(R.id.logout_menu)?.isVisible = true
+//            menu?.findItem(R.id.login_menu)?.isVisible = false
+//        }
+//        else
+//        {
+//            menu?.findItem(R.id.logout_menu)?.isVisible = false
+//            menu?.findItem(R.id.login_menu)?.isVisible = true
+//        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
+    }
+
 }
